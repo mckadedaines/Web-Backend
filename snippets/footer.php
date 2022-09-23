271700a72c0f570a583e9def1d32551992a3fa92
Mckade Daines <mckadedaines1@gmail.com>
2022-09-23 10:13:09 -0600

    header & nav done

--- snippets/footer.php
+++ snippets/footer.php
@@ -1,5 +1,5 @@
 <?php
     echo "<p class='footer'>&copy; PHP Motors, All rights reserved.
     All images used are believed to be in 'Fair Use'. Please notify the author if any are not and they will be removed.
-    Last Updated: 21 September, 2022";
+    <br> Last Updated: 21 September, 2022";
 ?>
