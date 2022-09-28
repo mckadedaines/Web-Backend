539dca603910a8b7142bfb3e2c9f57e860c2ec23
Mckade Daines <mckadedaines1@gmail.com>
2022-09-27 19:54:12 -0600

    need help

--- phpmotors/home.php
+++ phpmotors/home.php
@@ -6,9 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/mobile.css">
+    <link rel="stylesheet" href="css/large.css">
     <title>Home Page</title>
 </head>
 <body>
+    <div class="back-img"></div>
+
     <header>
         <?php
             require_once $_SERVER['DOCUMENT_ROOT'] . '/snippets/header.php';
@@ -31,49 +34,53 @@
             <p class="info_p">Fuzzy dice!</p>
         </div>
 
-        <img id="car" src="images/delorean.jpg" alt="delorean">
+        <div id="car">
+            <img src="images/delorean.jpg" alt="delorean">
+        </div>
 
         <div class="container">
             <img id="button" src="images/site/own_today.png" alt="Own Today">
             </div>
 
-        <div id="review">
-            <h2>DMC Dolrean Reviews</h2>
-            <ul id="list">
-                <li class="bullet">"So fast its almost like travelingin time." (4/5)</li>
-                <li class="bullet">"Coolest ride on the road." (4/5)</li>
-                <li class="bullet">"I'm feeling like Marty Mcfly!" (5/5)</li>
-                <li class="bullet">"The most futuristic ride of our day." (4.5/5)</li>
-                <li class="bullet">"80's livin and I love it!" (5/5)</li>
-            </ul>
-        </div>
+        <div class="side_by_side">
+            <div id="review">
+                <h2>DMC Dolrean Reviews</h2>
+                <ul id="list">
+                    <li class="bullet">"So fast its almost like travelingin time." (4/5)</li>
+                    <li class="bullet">"Coolest ride on the road." (4/5)</li>
+                    <li class="bullet">"I'm feeling like Marty Mcfly!" (5/5)</li>
+                    <li class="bullet">"The most futuristic ride of our day." (4.5/5)</li>
+                    <li class="bullet">"80's livin and I love it!" (5/5)</li>
+                </ul>
+            </div>
 
-        <h2 id="upgrades">Delorean Upgrades</h2>
+            <h2 id="upgrades">Delorean Upgrades</h2>
 
-        <div id="upgrade_info">
-        <div class="img_spacing">    
-            <div class="upgrade_img">
-                <img class="img" src="images/upgrades/flux-cap.png" alt="flux cap">
+            <div id="upgrade_info">
+                <div class="img_spacing">    
+                    <div class="upgrade_img">
+                        <img class="img" src="images/upgrades/flux-cap.png" alt="flux cap">
+                    </div>
+                    <a class="upgrade_links" href="">Flux Capacitor</a>
+                </div>
+                <div class="img_spacing">
+                    <div class="upgrade_img">
+                        <img class="img" src="images/upgrades/flame.jpg" alt="flame decals">
+                    </div>
+                    <a class="upgrade_links" href="">Flame Decals</a>
+                </div>
+                <div class="img_spacing">
+                    <div class="upgrade_img">
+                        <img class="img" src="images/upgrades/bumper_sticker.jpg" alt="bumper sticker">
+                    </div>
+                    <a class="upgrade_links" href="">Bumper Stickers</a>
+                </div>
+                <div class="img_spacing">
+                    <div class="upgrade_img">
+                        <img class="img" src="images/upgrades/hub-cap.jpg" alt="hub cap">
+                    </div>
+                    <a class="upgrade_links" href="">Hub Caps</a>
             </div>
-            <a class="upgrade_links" href="">Flux Capacitor</a>
-        </div>
-        <div class="img_spacing">
-            <div class="upgrade_img">
-                <img class="img" src="images/upgrades/flame.jpg" alt="flame decals">
-            </div>
-            <a class="upgrade_links" href="">Flame Decals</a>
-        </div>
-        <div class="img_spacing">
-            <div class="upgrade_img">
-                <img class="img" src="images/upgrades/bumper_sticker.jpg" alt="bumper sticker">
-            </div>
-            <a class="upgrade_links" href="">Bumper Stickers</a>
-        </div>
-        <div class="img_spacing">
-            <div class="upgrade_img">
-                <img class="img" src="images/upgrades/hub-cap.jpg" alt="hub cap">
-            </div>
-            <a class="upgrade_links" href="">Hub Caps</a>
         </div>
     </main>
 
