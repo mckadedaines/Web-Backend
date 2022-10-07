43c16079849896808bf780c74136afb16a657b6a
Mckade Daines <mckadedaines1@gmail.com>
2022-10-07 10:05:38 -0600

    almost done

--- phpmotors/library/connections.php
+++ phpmotors/library/connections.php
@@ -1,21 +1,23 @@
 <?php
+/*
+  Proxy connection to PHP motors Database
+*/
 function phpmotorsConnect(){
-    $server = 'localhost';
-    $dbname = 'phpmotors';
-    $username = 'iClient';
-    $password = 'uhrVpr1UuyZmri!J';
-    $dsn = "mysql:host=$server;dbname=$dbname";
-    $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
-
-    try {
-        $link = new PDO($dsn, $username, $password, $options);
-        return $link;
-    }
-    catch (PDOException $e) {
-        header('location: /phpmotors/view/500.php');
-        exit;
-    }
+  $server = 'localhost:3308';
+  $dbname = 'phpmotors';
+  $username = 'iClient';
+  $password = 'uhrVpr1UuyZmri!J';
+  $dsn = "mysql:host=$server;dbname=$dbname";
+  $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+  try{
+    $link = new PDO($dsn, $username, $password, $options);
+    // echo 'Worked';
+    return $link;
+  } catch(PDOException $e){
+    // echo "It didn't work, error: " . $e->getMessage();
+    header('Location: /phpmotors/view/500.php');
+  }
 }
-
+//For Testing Purposes Only
 phpmotorsConnect();
 ?>
--- phpmotors/sql/enhancement2.sql
+++ phpmotors/sql/enhancement2.sql
@@ -8,4 +8,10 @@ UPDATE inventory
     SET invDescription = REPLACE(invDescription, 'small', 'spacious')
     WHERE invModel = 'hummer';
 
- 
+SELECT inventory.invModel, carclassification.classificationName
+FROM inventory
+INNER JOIN carclassification on inventory.classificationId = carclassification.classificationId
+WHERE carclassification.classificationName = 'SUV';
+
+DELETE FROM inventory WHERE inventory.invId = '1';
+
